9a7890566fda31092646a9615d4e7f0c30ab1187
kumarkanchanleos <kumarkanchan.leos@gmail.com>
2024-09-21 15:30:34 +0530

    task_assignment1

---
+++ Ex_15092024/Lab_006.py
@@ -0,0 +1,17 @@
+#calculate the square and cube of the given number
+#lets say number is
+
+# number = 2
+# number1 = number ** 2
+# number2 = number ** 3
+# print(number1)
+# print(number2)
+
+
+#num1 then print number1 otherwise print number2
+number = float(input("please enter number \n"))
+number1 = float(input("please enter number 2\n"))
+if number > number1:
+   print("THE DIFF OF NUMBERS",number -number1)
+else:
+   print("this sum of the numbers is:",number1+number)
